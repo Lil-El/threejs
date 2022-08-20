7ceced2cad8fb16f57a6d71b4ef20f7f92879f94
Lil-El <yxd99324@qq.com>
2022-08-20 23:21:51 +0800

    add orbit and plane to box

--- utils/orbit.js
+++ utils/orbit.js
@@ -1,5 +1,8 @@
 import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 export function orbitControl(camera, dom) {
-    new OrbitControls(camera, dom);
+    const orbitControl = new OrbitControls(camera, dom);
+    orbitControl.enableDamping  = true; // 惯性阻尼
+    orbitControl.maxPolarAngle = 1.5; // 垂直旋转的角度的上限 (PI / 2) 90度；为了plane可以看到取1.5
+    return orbitControl;
 };
