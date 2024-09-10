c7ce200d74b5722b3699cea616d118bd4daa3aca
Lil-El <yxd99324@qq.com>
2024-09-10 20:30:34 +0800

    create base egg

--- examples/egg/index.js
+++ examples/egg/index.js
@@ -1,7 +1,29 @@
+import * as THREE from "three";
 import { createScene, createCube } from "lib";
 
 const scene = createScene();
 
-createCube(scene);
+// createCube(scene);
 
+const yolk = createYolk();
+const albumen = createAlbumen();
 
+scene.add(yolk);
+scene.add(albumen);
+
+function createYolk() {
+  const geometry = new THREE.CircleGeometry(2, 20);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z = 0.1;
+
+  return mesh;
+}
+
+function createAlbumen() {
+  const geometry = new THREE.CircleGeometry(4, 40);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  return mesh;
+}
